441a56788f1ae1596841b559afeaf7b252547456
Nikita Vetluzhskikh <trgrusted@gmail.com>
2025-06-24 12:24:35 +0200

    add start script

---
+++ start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+PROJECT_DIR="$(pwd)"
+BUILD_DIR="$PROJECT_DIR/cmake-build-debug"
+EXT_DIR="$PROJECT_DIR/external/test-VSCode-external-base"
+CLIENT_DIR="$EXT_DIR/client"
+
+CMAKE_FLAGS=""
+if [ "$1" == "-d" ]; then
+	echo "Debug mode enabled"
+  CMAKE_FLAGS="-DDEBUGING=ON"
+fi
+
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+cmake $CMAKE_FLAGS ..
+make
+
+cd "$EXT_DIR"
+
+[ ! -d node_modules ] && npm install
+[ ! -d client/node_modules ] && cd client && npm install && cd ..
+
+npx tsc -b
+
+code $EXT_DIR
+
+cd "$EXT_DIR"
